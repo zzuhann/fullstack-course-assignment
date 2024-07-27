a35c6c1349d4e43e37a97c65e4d3a942cb325675
zzuhann <tzuhan.lin@linego.me>
2024-07-27 22:19:31 +0800

    feat: add part2 example code

---
+++ part2/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.use(express.json());
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  console.log(note);
+  res.json(note);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
