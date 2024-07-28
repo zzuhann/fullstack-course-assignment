3a36d43eec8aaa1db78accee89a7c597dfe89a6a
zzuhann <zzuhanlin@gmail.com>
2024-07-28 21:46:27 +0800

    chore: add countries project

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        find countries <input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
