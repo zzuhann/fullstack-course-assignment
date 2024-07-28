59096f91442b34fab72c06d5f045c60724e8cf75
zzuhann <zzuhanlin@gmail.com>
2024-07-28 15:51:57 +0800

    feat: add function to add person

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,33 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleChangeNewName = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handleChangeNewName} value={newName} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addPerson}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   );
 };
