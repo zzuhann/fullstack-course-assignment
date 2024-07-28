a5ee3a43d6b8596fa5912742e93b4b670dbb269c
zzuhann <zzuhanlin@gmail.com>
2024-07-28 18:39:57 +0800

    fix: newPerson phone value and add id

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -44,7 +44,12 @@ const App = () => {
       window.alert(`${newName} is already added to phoneBook`);
       return;
     }
-    setPersons(persons.concat({ name: newName, phone: setNewNumber }));
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(newPerson));
     setNewName("");
     setNewNumber("");
   };
